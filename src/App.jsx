ab5285227497bb0cbda3b5a94d204133ef106b36
Jack Ball <ball.jack2376@gmail.com>
2023-08-21 14:57:15 -0600

    back of cards done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,42 @@
+import playerData from "./playerData";
+
+
 function BaseballCard(props) {
+  const statsDisplay = [];
+  for(let stat in props.stats){
+    statsDisplay.push(
+      <p>{`${stat}: ${props.stats[stat]}`}</p>
+    )
+  }
+
   return (
     <div className="card">
-      <h2>Player Name goes here</h2>
+      <h2>{props.name}</h2>
+      <p>{props.team}</p>
+      <p>{props.position}</p>
+      <p>{statsDisplay}</p>
     </div>
-  );
+  )
+  
+  // return (
+  //   <div className="card">
+  //     <h2>{props.name}</h2>
+  //     <img src={props.imgUrl}/>
+  //   </div>
+  // );
 }
 
 function App() {
-  return <BaseballCard />;
+  const cards = playerData.map((player) => 
+  <BaseballCard 
+  name={player.name}
+  team={player.team}
+  position={player.position}
+  stats={player.stats}
+  imgUrl={player.imgUrl}
+  key={player.cardId}
+  />);
+  return <>{cards}</>
 }
 
 export default App;
